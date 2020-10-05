339e348e8da7fc42c5035a5eab7c8811bcc56d99
CuteCorgi <55479773+Ban123456789@users.noreply.github.com>
2020-10-05 22:43:37 +0800

    已成功連上firebase database

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.engine('ejs', require('express-ejs-extend'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
